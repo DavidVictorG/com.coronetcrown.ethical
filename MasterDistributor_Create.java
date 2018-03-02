6408d63a769152d3956a7bf7efe161756bac0101
David Giandly <davidedp.coronet@gmail.com>
2018-03-02 09:10:29 +0700

    coba lagi

---
+++ MasterDistributor_Create.java
@@ -0,0 +1,13 @@
+package com.example.steven.comcoronetcrownethical;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MasterDistributor_Create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master_distributor__create);
+    }
+}
